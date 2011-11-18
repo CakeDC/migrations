d58578b7d1bc47cc0ef1294b14a621b61e032b76
Renan Gonçalves <renan.saddam@gmail.com>
2011-11-18 10:51:46 +0100

    Adding a couple of more checks to make sure everything is fine.

--- Config/Migration/002_convert_version_to_class_names.php
+++ Config/Migration/002_convert_version_to_class_names.php
@@ -33,12 +33,28 @@ class M4ec50d1f7a284842b1b770fdcbdd56cb extends CakeMigration {
 	);
 
 /**
+ * MigrationVersion instance
+ *
+ * @var MigrationVersion
+ * @access public
+ */
+	public $Version;
+
+/**
  * Records to be migrated
  *
  * @var array
  * @access public
  */
 	public $records = array();
+
+/**
+ * Mappings to the records
+ *
+ * @var array
+ * @access public
+ */
+	public $mappings = array();
 
 /**
  * Before migration callback
@@ -52,10 +68,17 @@ class M4ec50d1f7a284842b1b770fdcbdd56cb extends CakeMigration {
 			throw new InternalErrorException(__d('migrations', 'Sorry, I can\'t downgrade. Why would you want that anyways?'));
 		}
 
-		$this->records = $this->generateModel('Version', 'schema_migrations')->find('all');
+		$this->Version = new MigrationVersion(array(
+			'connection' => $this->connection,
+			'autoinit' => false
+		));
+		$this->Version->Version = $this->generateModel('Version', 'schema_migrations');
+
+		$this->records = $this->Version->Version->find('all');
 
 		$this->needsUpgrade();
 		$this->checkPlugins();
+		$this->checkRecords();
 
 		return true;
 	}
@@ -68,26 +91,7 @@ class M4ec50d1f7a284842b1b770fdcbdd56cb extends CakeMigration {
  * @access public
  */
 	public function after($direction) {
-		$Model = $this->generateModel('Version', 'schema_migrations');
-		$Version = new MigrationVersion(array(
-			'connection' => $this->connection,
-			'autoinit' => false
-		));
-		$Version->Version = $Model;
-
-		$mappings = array();
-		foreach ($this->records as $record) {
-			$type = $record['Version']['type'];
-			if (!isset($mappings[$type])) {
-				$mappings[$type] = $Version->getMapping($type);
-			}
-			$mapping = $mappings[$type];
-			$migration = $mapping[$record['Version']['version']];
-
-			$Model->id = $record['Version']['id'];
-			$Model->saveField('version', $migration['class']);
-		}
-		die;
+		$this->upgradeRecords();
 
 		return true;
 	}
@@ -98,7 +102,7 @@ class M4ec50d1f7a284842b1b770fdcbdd56cb extends CakeMigration {
  * @return void
  */
 	public function needsUpgrade() {
-		$schema = $this->generateModel('Version', 'schema_migrations')->schema();
+		$schema = $this->Version->Version->schema();
 
 		// Needs upgrade
 		if ($schema['version']['type'] === 'integer') {
@@ -106,12 +110,13 @@ class M4ec50d1f7a284842b1b770fdcbdd56cb extends CakeMigration {
 		}
 		
 		// Do not need, 001 already set it as string
-		// Unset actions and records, so it wont try again
+		// Unset actions, records and mappings, so it wont try again
 		$this->migration = array(
 			'up' => array(),
 			'down' => array()
 		);
 		$this->records = array();
+		$this->mappings = array();
 	}
 
 /**
@@ -122,16 +127,64 @@ class M4ec50d1f7a284842b1b770fdcbdd56cb extends CakeMigration {
  */
 	public function checkPlugins() {
 		$types = Set::extract('/Version/type', $this->records);
-		$types = array_unique($types);
+		$types = $plugins = array_unique($types);
 
 		// Remove app from it
-		$index = array_search('app', $types);
+		$index = array_search('app', $plugins);
 		if ($index !== false) {
-			unset($types[$index]);
+			unset($plugins[$index]);
 		}
 
 		// Try to load them
-		CakePlugin::load($types);
+		CakePlugin::load($plugins);
+	}
+
+/**
+ * Check if the version is present in the mappings
+ *
+ * @throws RuntimeException, MigrationVersionException
+ * @return void
+ */
+	public function checkRecords() {
+		foreach ($this->records as $record) {
+			$type = $record['Version']['type'];
+			$version = $record['Version']['version'];
+
+			if (!isset($this->mappings[$type])) {
+				$this->mappings[$type] = $this->Version->getMapping($type);
+			}
+			
+			// Existing mapping
+			if (empty($this->mappings[$type][$version])) {
+				throw new RuntimeException(sprintf(
+					'Not able to match version `%d` in `%s`. Make sure every migration applied to the database is present on the filesystem.',
+					$version, $type
+				));
+			}
+
+			// Migration file and class present? If not, will throw an exception
+			$info = $this->mappings[$type][$version];
+			$migration = $this->Version->getMigration($info['name'], $info['class'], $type);
+			unset($migration);
+		}
+	}
+
+/**
+ * Upgrade records, setting version as class name
+ *
+ * @return void
+ */
+	public function upgradeRecords() {
+		foreach ($this->records as $record) {
+			$type = $record['Version']['type'];
+			$version = $record['Version']['version'];
+
+			$mapping = $this->mappings[$type];
+			$migration = $mapping[$version];
+
+			$this->Version->Version->id = $record['Version']['id'];
+			$this->Version->Version->saveField('version', $migration['class']);
+		}
 	}
 }
 ?>
