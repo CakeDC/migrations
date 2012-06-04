8a9cc5d5cb698ceaf5c3b03694564e05640f4f00
Tommy Goode <airdrummingfool+github@gmail.com>
2012-06-04 18:50:36 -0400

    Created a migration file to change the class name field length to 255. Added a warning in the console if the user inputs a description longer than 255 characters.

---
+++ Config/Migration/003_increase_class_name_length.php
@@ -0,0 +1,58 @@
+<?php
+class IncreaseClassNameLength extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = 'Increase the maximum length of class names.';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'alter_field' => array(
+				'schema_migrations' => array(
+					'class' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 255, 'name' => 'class')
+				)
+			)
+		),
+		'down' => array(
+			'alter_field' => array(
+				'schema_migrations' => array(
+					'class' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 33, 'name' => 'class')
+				)
+			)
+		)
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
+
+?>
