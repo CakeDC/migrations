c772134a763271fafbb23c4977ad234bbc4989ab
Guillermo Mansilla <mansillag@gmail.com>
2012-08-16 15:10:16 -0430

    Fix unit tests for issue#26 and assertions after the new migration for the plugin itself

--- Test/Case/Console/Command/MigrationShellTest.php
+++ Test/Case/Console/Command/MigrationShellTest.php
@@ -805,9 +805,9 @@ TEXT;
 /Migrations Plugin
 
 Current version:
-  #002 002_convert_version_to_class_names
+  #003 003_increase_class_name_length
 Latest version:
-  #002 002_convert_version_to_class_names/
+  #003 003_increase_class_name_length/
 TEXT;
 		$this->assertPattern(str_replace("\r\n", "\n", $pattern), $result);
 
@@ -817,7 +817,7 @@ TEXT;
 		$result = $this->Shell->output;
 		$this->assertNoPattern(str_replace("\r\n", "\n", $pattern), $result);
 
-		$this->Shell->Version->setVersion(2, 'migrations', false);
+		$this->Shell->Version->setVersion(3, 'migrations', false);
 		$this->Shell->output = '';
 		$this->Shell->args = array('outdated');
 		$this->Shell->status();
@@ -826,9 +826,9 @@ TEXT;
 /Migrations Plugin
 
 Current version:
-  #001 001_init_migrations
+  #002 002_convert_version_to_class_names
 Latest version:
-  #002 002_convert_version_to_class_names/
+  #003 003_increase_class_name_length/
 TEXT;
 		$this->assertPattern(str_replace("\r\n", "\n", $pattern), $result);
 		$this->Shell->Version->setVersion(1, 'migrations');
