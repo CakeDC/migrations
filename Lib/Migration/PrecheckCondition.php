815455a66305b0a3618e1b3ce8d5e9961d2c9f4c
Florian Krämer <florian.kraemer@cakedc.com>
2014-03-04 13:43:32 +0100

    Updating the doc blocks

--- Lib/Migration/PrecheckCondition.php
+++ Lib/Migration/PrecheckCondition.php
@@ -1,17 +1,12 @@
 <?php 
 /**
- * CakePHP Migrations
- *
- * Copyright 2009 - 2013, Cake Development Corporation
- *						1785 E. Sahara Avenue, Suite 490-423
- *						Las Vegas, Nevada 89104
+ * Copyright 2009 - 2014, Cake Development Corporation (http://cakedc.com)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright 2009 - 2013, Cake Development Corporation
- * @link	  http://codaset.com/cakedc/migrations/
- * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @copyright Copyright 2009 - 2014, Cake Development Corporation (http://cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
 App::uses('PrecheckBase', 'Migrations.Lib/Migration');
