460c5e015a8cdbd093d8248eca669e9911b3a035
Jose Alejandro Alvarez Mendoza <jose.mendoza@cakedc.com>
2015-07-17 17:24:46 -0430

    adding database to schema.php comparison option

--- Console/Command/MigrationShell.php
+++ Console/Command/MigrationShell.php
@@ -389,6 +389,7 @@ class MigrationShell extends AppShell {
 		$migration = array('up' => array(), 'down' => array());
 		$migrationName = '';
 		$comparison = array();
+
 		if (!empty($this->args)) {
 			// If args are passed in from the command line, we just want to
 			// generate a migration based on them - don't offer to compare to database
@@ -399,12 +400,15 @@ class MigrationShell extends AppShell {
 				$response = $this->in(__d('migrations', 'Do you want to compare the schema.php file to the database?'), array('y', 'n'), 'y');
 				if (strtolower($response) === 'y') {
 					$this->_generateFromComparison($migration, $oldSchema, $comparison);
-					if (empty($comparison)) {
-						$this->hr();
-						$this->out(__d('migrations', 'No database changes detected.'));
-						return $this->_stop();
-					}
+					$this->_comparisonChanges($comparison);
 					$fromSchema = true;
+				} else {
+					$response = $this->in(__d('migrations', 'Do you want to compare the database to the schema.php file?'), array('y', 'n'), 'y');
+					if(strtolower($response) === 'y') {
+						$this->_generateFromInverseComparison($migration, $oldSchema, $comparison);
+						$this->_comparisonChanges($comparison);
+						$fromSchema = false;
+					}
 				}
 			} else {
 				$response = $this->in(__d('migrations', 'Do you want to generate a dump from the current database?'), array('y', 'n'), 'y');
@@ -420,8 +424,16 @@ class MigrationShell extends AppShell {
 		if ($fromSchema && isset($comparison)) {
 			$response = $this->in(__d('migrations', 'Do you want to update the schema.php file?'), array('y', 'n'), 'y');
 			if (strtolower($response) === 'y') {
-				$this->_updateSchema($comparison);
+				$this->_updateSchema();
 			}
+		}
+	}
+
+	protected function _comparisonChanges(&$comparison) {
+		if (empty($comparison)) {
+			$this->hr();
+			$this->out(__d('migrations', 'No database changes detected.'));
+			return $this->_stop();
 		}
 	}
 
@@ -441,9 +453,28 @@ class MigrationShell extends AppShell {
 			unset($oldSchema->tables['schema_migrations']);
 		}
 		$newSchema = $this->_readSchema();
-		$comparison = $this->Schema->compare($oldSchema->tables, $newSchema['tables']['missing']);
-		unset($comparison['schema_migrations']);
-		$migration = $this->_fromComparison($migration, $comparison, $oldSchema->tables, $newSchema['tables']['missing']);
+		$comparison = $this->Schema->compare($oldSchema, $newSchema);
+		$migration = $this->_fromComparison($migration, $comparison, $oldSchema->tables, $newSchema['tables']);
+	}
+
+/**
+ * Generate a migration by comparing the database with schema.php.
+ *
+ * @param array &$migration Reference to variable of the same name in generate() method
+ * @param array &$oldSchema Reference to variable of the same name in generate() method
+ * @param array &$comparison Reference to variable of the same name in generate() method
+ * @return void (The variables passed by reference are changed; nothing is returned)
+ */
+	protected function _generateFromInverseComparison(&$migration, &$oldSchema, &$comparison) {
+		$this->hr();
+		$this->out(__d('migrations', 'Comparing database to the schema.php...'));
+
+		if ($this->type !== 'migrations') {
+			unset($oldSchema->tables['schema_migrations']);
+		}
+		$database = $this->_readSchema();
+		$comparison = $this->Schema->compare($database, $oldSchema);
+		$migration = $this->_fromComparison($migration, $comparison, $oldSchema->tables, $database['tables']);
 	}
 
 /**
@@ -789,20 +820,15 @@ class MigrationShell extends AppShell {
  *
  * @return void
  */
-	protected function _updateSchema(&$comparison) {
-		if (!empty($comparison)) {
-			$command = 'schema generate -f -m --exclude schema_migrations';
+	protected function _updateSchema() {
+		$command = 'schema generate --connection ' . $this->connection;
+		if (!empty($this->params['plugin'])) {
+			$command .= ' --plugin ' . $this->params['plugin'];
 		}
-		else {
-			$command = 'schema generate --connection ' . $this->connection;
-			if (!empty($this->params['plugin'])) {
-				$command .= ' --plugin ' . $this->params['plugin'];
-			}
-			if ($this->params['force']) {
-				$command .= ' --force';
-			}
-			$command .= ' --file schema.php --name ' . $this->_getSchemaClassName($this->type, false);
+		if ($this->params['force']) {
+			$command .= ' --force';
 		}
+		$command .= ' --file schema.php --name ' . $this->_getSchemaClassName($this->type, false);
 		$this->dispatchShell($command);
 	}
 
