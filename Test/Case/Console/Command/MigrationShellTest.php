0fa9fa0f95334e8c815a6583533c81febfc135e5
Jose Alejandro Alvarez Mendoza <jose.mendoza@cakedc.com>
2015-08-15 13:42:24 -0430

    Adding unit test for comparing database with schema.php file

--- Test/Case/Console/Command/MigrationShellTest.php
+++ Test/Case/Console/Command/MigrationShellTest.php
@@ -826,6 +826,47 @@ TEXT;
 	}
 
 /**
+ * TestGenerateInverseComparison method
+ *
+ * @return void
+ */
+	public function testGenerateInverseComparison() {
+		$this->Shell->type = 'TestMigrationPlugin4';
+		$this->Shell->expects($this->at(0))->method('in')->will($this->returnValue('n'));
+		$this->Shell->expects($this->at(1))->method('in')->will($this->returnValue('y'));
+		$this->Shell->expects($this->at(3))->method('in')->will($this->returnValue('n'));
+		$this->Shell->expects($this->at(4))->method('in')->will($this->returnValue('create slug field'));
+
+		$this->Shell->Version->expects($this->any())->method('getMapping')->will($this->returnCallback(array($this, 'returnMapping')));
+
+		$this->assertEmpty(glob(TMP . 'tests' . DS . '*create_slug_field.php'));
+		$this->Shell->params['force'] = true;
+		$this->Shell->generate();
+		$files = glob(TMP . 'tests' . DS . '*create_slug_field.php');
+		$this->assertNotEmpty($files);
+
+		$result = $this->_getMigrationVariable(current($files));
+		$this->_unlink($files);
+		$this->assertNotRegExp('/\'schema_migrations\'/', $result);
+
+		$pattern = <<<TEXT
+/			'create_field' => array\(
+				'articles' => array\(
+					'slug' => array\('type' => 'string', 'null' => false, 'after' => 'title'\),
+				\),
+			\),/
+TEXT;
+		$this->assertRegExp(str_replace("\r\n", "\n", $pattern), $result);
+
+		$pattern = <<<TEXT
+/			'drop_field' => array\(
+				'articles' => array\('slug'\),
+			\),/
+TEXT;
+		$this->assertRegExp(str_replace("\r\n", "\n", $pattern), $result);
+	}
+
+/**
  * testGenerateFromCliParamsCreateTable method
  * test the case of using a command such as:
  * app/Console/cake Migrations.migration generate create_products id created modified name description:text in_stock:boolean price:float stock_count:integer
@@ -1015,4 +1056,3 @@ TEXT;
 	}
 
 }
-
