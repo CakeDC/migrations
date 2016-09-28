03256d828f283632f0379510575582eb3cf5ac30
Alejandro Ibarra <alejandro.ibarra@cakedc.com>
2016-09-28 09:51:09 -0500

    Revert "Fix issue with extends" in favor of #275
    This reverts commit eab921cb9c47a6790a60a87151c06d6a3c5a034e.

--- Lib/CakeMigration.php
+++ Lib/CakeMigration.php
@@ -14,7 +14,7 @@ App::uses('CakeSchema', 'Model');
 /**
  * Base Class for Migration management
  */
-class CakeMigration {
+class CakeMigration extends Object {
 
 /**
  * Migration description
@@ -168,6 +168,7 @@ class CakeMigration {
  * @throws MigrationException
  */
 	public function __construct($options = array()) {
+		parent::__construct();
 
 		if (!isset($options['dry'])) {
 			$options['dry'] = false;
